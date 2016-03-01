b6272cee7ecde56d88c9059d8a00aa0b854b151f
Nathan Good <nathgood@amazon.com>
2016-03-01 13:24:23 -0600

    Initial import

---
+++ java-webapp/src/main/java/com/amazon/aws/CodecommitCodepipelineApplication.java
@@ -0,0 +1,12 @@
+package com.amazon.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecommitCodepipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecommitCodepipelineApplication.class, args);
+	}
+}
